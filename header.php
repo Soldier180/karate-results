25ace293f0e888e85fb50ff96799f0e569575a4c
denisqa <dlioon3@gmail.com>
2015-12-24 20:16:07 +0200

    CSS/JS folders

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <html>
 	<head>
 		<title>Title</title>
+		<link rel="stylesheet" href="/css/style.css" />
+		<script src="/js/jquery-2.1.4.js"></script>
 	</head>
 	<body>
 		<?php echo "Hello"; ?>
